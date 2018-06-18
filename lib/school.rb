f2259fedfbc79235002cbfc9b3ee5105bf22f131
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-18 20:55:24 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,7 @@ class School
   def add_student(student,grade)
     @roster = roster
     @grade = grade
-    roster[:grade] = []
-    roster[:grade] << student
+    roster[grade] = []
+    roster[grade] << student
 end
 end
